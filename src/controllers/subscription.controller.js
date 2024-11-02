35497b5fa1ddfb66ee90f7d921b7a9e309e97291
DevloperMM <mangalmurtivarshney2810@gmail.com>
2024-11-02 11:48:02 +0530

    Completed and tested subscription controllers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,20 +2,155 @@ import mongoose from "mongoose";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Subscription } from "../models/subscription.models.js";
+import { User } from "../models/user.models.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
-  // TODO: toggle subscription
+  const user = await User.findById(channelId);
+  if (!user) {
+    throw new ApiError(404, "No such channel found");
+  }
+
+  // const result = await Subscription.aggregate([
+  //   {
+  //     $match: {
+  //       channel: new mongoose.Types.ObjectId(channelId),
+  //       subscriber: new mongoose.Types.ObjectId(req.user._id),
+  //     },
+  //   },
+  // ]);
+
+  const channelSubscriber = await Subscription.findOne({
+    subscriber: req.user._id,
+    channel: channelId,
+  });
+
+  try {
+    if (channelSubscriber) {
+      const response = await Subscription.findByIdAndDelete(
+        channelSubscriber._id
+      );
+      return res
+        .status(200)
+        .json(new ApiResponse(200, response, "Unsubscribed successfully"));
+    } else {
+      const response = await Subscription.create({
+        subscriber: req.user._id,
+        channel: channelId,
+      });
+      return res
+        .status(200)
+        .json(new ApiResponse(200, response, "Subscribed successfully"));
+    }
+  } catch (err) {
+    throw new ApiError(
+      500,
+      err?.message || "Error occured to subscribe/unsubscribe"
+    );
+  }
 });
 
-const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+const getUserSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
-  // TODO: get subscriber list of a channel
+
+  const isChannel = await User.exists({ _id: channelId });
+  if (!isChannel) {
+    throw new ApiError(404, "No such channel found");
+  }
+
+  try {
+    const subscribers = await Subscription.aggregate([
+      {
+        $match: { channel: new mongoose.Types.ObjectId(channelId) },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "subscriber",
+          foreignField: "_id",
+          as: "subscribers",
+        },
+      },
+      {
+        $unwind: "$subscribers",
+      },
+      {
+        $project: {
+          subscribers: {
+            _id: 1,
+            username: 1,
+            fullName: 1,
+            email: 1,
+          },
+        },
+      },
+    ]);
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, subscribers, "Subscribers fetched successfully")
+      );
+  } catch (err) {
+    throw new ApiError(
+      500,
+      err?.message || "Error while retrieving subscribers"
+    );
+  }
 });
 
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
-  // TODO: get channel list to which user has subscribed
+
+  const isExist = await User.findById({ _id: subscriberId });
+  if (!isExist) {
+    throw new ApiError(404, "No such User found");
+  }
+
+  try {
+    const subscribedChannels = await Subscription.aggregate([
+      {
+        $match: { subscriber: new mongoose.Types.ObjectId(subscriberId) },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "channel",
+          foreignField: "_id",
+          as: "channels",
+        },
+      },
+      {
+        $unwind: "$channels",
+      },
+      {
+        $project: {
+          channels: {
+            _id: 1,
+            username: 1,
+            fullName: 1,
+            avatar: 1,
+          },
+        },
+      },
+    ]);
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          subscribedChannels,
+          "Subscriptions fetched successfully"
+        )
+      );
+  } catch (err) {
+    throw new ApiError(
+      500,
+      err?.message || "Error while retrieving subscriptions"
+    );
+  }
 });
 
-export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
+export { toggleSubscription, getSubscribedChannels, getUserSubscribers };
--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import {
+  getUserSubscribers,
   getSubscribedChannels,
-  getUserChannelSubscribers,
   toggleSubscription,
 } from "../controllers/subscription.controller.js";
 import { verifyJWT } from "../middlewares/auth.midware.js";
@@ -9,11 +9,8 @@ import { verifyJWT } from "../middlewares/auth.midware.js";
 const router = Router();
 router.use(verifyJWT);
 
-router
-  .route("/c/:channelId")
-  .get(getSubscribedChannels)
-  .post(toggleSubscription);
+router.route("/c/:channelId").get(getUserSubscribers).post(toggleSubscription);
 
-router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+router.route("/u/:subscriberId").get(getSubscribedChannels);
 
 export default router;
